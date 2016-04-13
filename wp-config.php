16388383bc3c0e29401e20ea8b72a440e1cd0c3d
zaiats85 <zaiats85@localhost>
2016-04-13 17:40:25 +0300

    changed db credits

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'skipass');
+define('DB_NAME', 'dev_skipass');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'ozayats');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'zgoba-90');
+define('DB_PASSWORD', 'HNf*we8_1ewf');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '192.168.0.105');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
